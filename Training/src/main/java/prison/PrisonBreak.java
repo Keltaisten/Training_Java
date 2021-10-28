93439a0cf1a3c5427a0131a6ccd5cb809f74f31a
Keltaisten <keltaisten@gmail.com>
2021-10-28 13:45:28 +0200

    PrisonBreak_update_2

--- Training/src/main/java/prison/PrisonBreak.java
+++ Training/src/main/java/prison/PrisonBreak.java
@@ -5,30 +5,22 @@ import java.util.List;
 
 public class PrisonBreak {
     int numberOfDoors = 100;
-    List<Boolean> doors = new ArrayList<>(numberOfDoors);
     List<Integer> openDoors = new ArrayList<>();
 
-    public void closeTheDoors(){
-        for(int i = 0; i<numberOfDoors;i++){
-            doors.set(i, false);
-        }
-    }
     public void openTheDoors(){
-        for(int i = 1; i<numberOfDoors;i++){
-            if(i*i<numberOfDoors) {
+        for(int i = 1; i<=numberOfDoors;i++){
+            if(i*i<=numberOfDoors) {
                 openDoors.add(i * i);
             }
+            else{
+                break;
+            }
         }
-//        return openDoors;
     }
-
 
     public static void main(String[] args) {
         PrisonBreak prisonBreak = new PrisonBreak();
-//        prisonBreak.closeTheDoors();
         prisonBreak.openTheDoors();
         System.out.println(prisonBreak.openDoors);
-
-
     }
 }
